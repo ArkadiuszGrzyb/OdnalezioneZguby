cac806452ff9e8a2eec195bdffcb2770deef84fb
arkadiusz <arkadiusz.grzyb.it@gmail.com>
2025-12-06 12:54:08 +0100

    Initial commit for local development

---
+++ src/main/java/org/example/odnalezionezguby/OdnalezioneZgubyApplication.java
@@ -0,0 +1,13 @@
+package org.example.odnalezionezguby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdnalezioneZgubyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdnalezioneZgubyApplication.class, args);
+    }
+
+}
