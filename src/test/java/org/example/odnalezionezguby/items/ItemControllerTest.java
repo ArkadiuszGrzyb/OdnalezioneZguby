6e01ad1c3bf8a14d926c982eabfeacc61d4acd09
arkadiusz <arkadiusz.grzyb.it@gmail.com>
2026-02-22 03:12:29 +0100

    Web Layer Unit Tests added for updateItem

--- src/test/java/org/example/odnalezionezguby/items/ItemControllerTest.java
+++ src/test/java/org/example/odnalezionezguby/items/ItemControllerTest.java
@@ -1,12 +1,15 @@
 package org.example.odnalezionezguby.items;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.mockito.Mockito.when;
@@ -19,6 +22,9 @@ public class ItemControllerTest {
 
     @MockBean
     private ItemService itemService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void getItemByName_returnsExpectedItem() throws Exception {
@@ -35,5 +41,38 @@ public class ItemControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name").value("Wallet"));
 
+    }
+
+    @Test
+    void updateItem_missingId_returnsBadRequest() throws Exception {
+        // given
+        Item itemWithoutId = new Item();
+        itemWithoutId.setName("Item without id");
+
+        //when & then
+        mockMvc.perform(put("/item")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(itemWithoutId)))
+                .andExpect(status().isBadRequest());
+    }
+
+
+    @Test
+    void updateItem_returnsUpdatedItem() throws Exception {
+        //given
+        Item itemToUpdate = new Item();
+        itemToUpdate.setId(1L);
+        itemToUpdate.setName("Updated");
+
+        //when
+        when(itemService.updateItem(itemToUpdate)).thenReturn(itemToUpdate);
+
+        //then
+        mockMvc.perform(put("/item")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(itemToUpdate)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Updated"));
     }
 }
